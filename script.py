c5188ed7bcd1d46bf98191145d97a898338ab1a2
Alexandr SARANDI <sarandi.alex@gmail.com>
2021-01-06 11:40:45 -0800

    config object

--- script.py
+++ script.py
@@ -8,8 +8,18 @@ import os
 from collections import Counter
 from PIL import Image, ImageDraw, ImageFont
 
+config = {
+    "window_max_size": 768,
+    "window_max_height": 768,
+    "screenshot_region": (0, 0, 353, 768),
+    "circle_center": (176, 590),
+    "circle_radius": 110,
+    "detected_min_height": 38,
+    "detected_min_width": 6,
+}
 
-def load_words():
+
+def load_words() -> set:
     with open("words.txt") as fp:
         res = set(fp.read().split())
         fp.close()
@@ -39,56 +49,62 @@ def make_letter_contours() -> {}:
 
 
 def detect_letters(letter_contours: {}, img: Image) -> {}:
+    global config
     img = cv.cvtColor(np.array(img), cv.COLOR_RGB2GRAY)
     img = cv.GaussianBlur(img, (3, 7), 0)
     img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)[1]
     mask = np.zeros(img.shape, np.uint8)
-    cv.circle(mask, (176, 590), 110, 1, -1)  # center coords, radius, color, thickness
+    cv.circle(mask, config["circle_center"], config["circle_radius"], 1, -1)
     img = cv.bitwise_and(img, img, mask=mask)
     img = cv.bitwise_not(img, img, mask=mask)
-    #    cv.imshow("as", img)
+    #    cv.imshow("img", img)
     #    cv.waitKey()
     contours, hierarchy = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     res = []
     for cnt1 in contours:
         x, y, w, h = cv.boundingRect(cnt1)
-        if h < 38 or w < 6:
+        if h < config["detected_min_height"] or w < config["detected_min_width"]:
             continue
         best_score, best_letter = 1, '_'
         for letter, cnt2 in letter_contours.items():
             score = cv.matchShapes(cnt1, cnt2, cv.CONTOURS_MATCH_I1, 0.0)
             if score < best_score:
                 best_score, best_letter = score, letter
-        res.append((best_letter, x + (w // 2), y + (h // 2)))
+        res.append((best_letter, best_score, x + (w // 2), y + (h // 2)))
     return res
 
 
 def join_letters(lst: [tuple]) -> str:
-    return "".join([x[0] for x in lst]).lower()
+    return "".join([x[0] for x in lst])
 
 
 def scrcpy():
-    cmd = """killall scrcpy ; \
-            nohup scrcpy \
-                --stay-awake \
-                --turn-screen-off \
-                --always-on-top \
-                --window-borderless \
-                --window-x 0 \
-                --window-y 0 \
-                --max-size 768 \
-                --window-height 768 \
-                --lock-video-orientation 0 \
-                >/dev/null 2>&1 &"""
+    global config
+    cmd = f"""
+                killall scrcpy;                                 \
+                nohup scrcpy                                    \
+                    --stay-awake                                \
+                    --turn-screen-off                           \
+                    --always-on-top                             \
+                    --window-borderless                         \
+                    --window-x 0                                \
+                    --window-y 0                                \
+                    --max-size {config['window_max_size']}      \
+                    --window-height {config['window_height']}   \
+                    --lock-video-orientation 0                  \
+                    >/dev/null 2>&1 &
+        """
     os.system(cmd)
 
 
 # scrcpy()
 all_letters = make_letter_contours()
 all_words = load_words()
-capture = pyautogui.screenshot(region=(0, 0, 353, 768)).convert("RGB")
+capture = pyautogui.screenshot(region=config["screenshot_region"]).convert("RGB")
 detected = detect_letters(all_letters, capture)
-results = match_words(all_words, join_letters(detected))
+for char in detected:
+    print(char)
+results = match_words(all_words, join_letters(detected).lower())
 results = sorted(results)
 results.sort(key=lambda x: len(x))
 print(results)
